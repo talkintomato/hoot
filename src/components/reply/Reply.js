fd6a93a84fec123808e61f9638791ef7c26ed44a
talkintomato <darinloh@gmail.com>
2021-05-29 14:22:46 +0800

    First Commit

--- src/components/reply/Reply.js
+++ src/components/reply/Reply.js
@@ -1,12 +1,48 @@
+import { Button, Card, makeStyles, Typography } from "@material-ui/core";
+const useStyles = makeStyles({
+  button: {
+    backgroundColor: "#EAAB66",
+    margin: "20px",
+  },
+  root: {
+    minWidth: 275,
+    padding: "50px",
+    margin: "100px",
+    background: "#000",
+    opacity: "70%",
+  },
+  title: {
+    fontSize: 40,
+    color: "#FFF",
+  },
+})
 
-import NavBar from '../Navbar';
 
 function Reply() {
+  const classes = useStyles();
+
   return (
     <>
-     <h1> Reply </h1>
-     </>
+      <h1> Reply </h1>
+      <Card className={classes.root}>
+        <Typography className={classes.title}>
+          Dear A,
+          Never gonna give you up.
+          Never gonna let you down.
+          Never gonna run around and desert you.
+          Never gonna make you cry.
+          Never gonna say goodbye.
+          Never gonna tell a lie and hurt you.
+       </Typography>
+      </Card>
+      <Button variant="contained" className={classes.button}> Previous </Button>
+      <Button variant="contained" className={classes.button}> Reply </Button>
+      <Button variant="contained" className={classes.button}> Next </Button>
+
+
+    </>
   );
 }
 
 export default Reply;
+
