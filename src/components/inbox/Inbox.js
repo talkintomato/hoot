2372a32c82b17f3a732ff7624a57b921049fb422
talkintomato <darinloh@gmail.com>
2021-06-01 20:30:38 +0800

    commit check

--- src/components/inbox/Inbox.js
+++ src/components/inbox/Inbox.js
@@ -1,9 +1,19 @@
-import NavBar from '../Navbar';
+import { Card, makeStyles } from "@material-ui/core";
+
+const UseStyles = makeStyles({
+    card: {
+        
+    }
+})
 
 function Inbox() {
+  const classes = UseStyles(); 
   return (
     <>
      <h1> Inbox </h1>
+     <Card className={classes.card}> 
+        
+     </Card>
      </>
   );
 }
