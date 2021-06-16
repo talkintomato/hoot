380f2a78cd8eaba8cba5f40d3f2782008b69db02
chinkiat <e0544173@u.nus.edu>
2021-06-17 03:12:19 +0800

    first draft of profile page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,44 @@
-import NavBar from '../Navbar';
+import NavBar from "../Navbar";
+import { Card, Grid, makeStyles, Typography } from "@material-ui/core";
+import owl1 from "./hootowl.jpg";
+import ProfileCard from "./ProfileCard";
+import ProfileData from "./ProfileData";
+
+const UseStyles = makeStyles({
+  profileBox: {
+    padding: "20px",
+    backgroundColor: "rgba(00, 00, 00, 0.6)",
+  },
+  profileName: {
+    fontSize: 55,
+    color: "white",
+  },
+  profileBody: {
+    fontSize: 36,
+    color: "white",
+  },
+  avatar: {
+    maxWidth: "100%",
+    maxHeight: "70%",
+    margin: "50px",
+    marginTop: "10px",
+  },
+});
 
 function Profile() {
+  const classes = UseStyles();
   return (
     <>
-     <h1> Profile </h1>
-     </>
+      <h1> Profile </h1>
+      <Grid container spacing={3}>
+        <Grid item xs={5}>
+          <img src={owl1} alt="" className={classes.avatar} />
+        </Grid>
+        <Grid item xs={6}>
+          <ProfileCard data={ProfileData} />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
