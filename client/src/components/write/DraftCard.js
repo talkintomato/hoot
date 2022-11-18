998c0cffd7edc6f64e9b7391636da7eb5006d244
chinkiat <e0544173@u.nus.edu>
2022-11-18 10:53:06 +0800

    revamped write page

--- client/src/components/write/DraftCard.js
+++ client/src/components/write/DraftCard.js
@@ -1,66 +1,86 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Divider } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Divider } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-    padding: "100",
-    margin: "100px",
-    background: "#000",
-    opacity: "70%",
+    padding: "10px",
+    margin: "25px",
+    borderStyle: "dotted",
+    borderColor: "#13C7C2",
+    background: "#D8ECEC",
   },
   title: {
-    fontSize: 40,
-    color: "#FFF",
+    fontFamily: "Comfortaa",
+    fontWeight: "bold",
+    fontSize: 25,
+    textTransform: "none",
   },
   pos: {
     marginBottom: 12,
   },
   divider: {
-    width: '100%',
-    backgroundColor: "#ffff",
+    height: "3px",
+    width: "96%",
+    margin: "0px",
+    opacity: "35%",
+    borderRadius: "20px",
+    backgroundColor: "#4b9b9b",
   },
-  messagePrev : {
-    width: '100%',
-    margin: "20px",
+  messagePrev: {
+    fontFamily: "Comfortaa",
+    fontWeight: "normal",
+    textTransform: "none",
+  },
+  messagePrevButton: {
+    marginTop: "15px",
     padding: "10px",
+    justifyContent: "flex-start",
+    width: "100%",
   },
-  compose : {
+  compose: {
     height: "100%",
     margin: "10px",
+    fontFamily: "Comfortaa",
+    fontWeight: "normal",
+    textTransform: "none",
   },
-  header : {
+  header: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-  }
+  },
 });
 
 export default function DraftCard(props) {
   const classes = useStyles();
   console.log(props.data);
 
-
   return (
-    <>
     <Card className={classes.root}>
       <CardContent className={classes.header}>
         <Typography className={classes.title} gutterBottom>
           Drafts
         </Typography>
-        <Button variant="contained" className= {classes.compose}> Compose </Button>
+        <Button variant="contained" className={classes.compose}>
+          {" "}
+          Compose{" "}
+        </Button>
       </CardContent>
-      <Divider className={classes.divider}/> 
-        {props.data.map((users) => (
-          <Button variant="contained" className={classes.messagePrev}> {users.content} </Button>
-        ))}
+      <Divider className={classes.divider} />
+      {props.data.map((users) => (
+        <Button variant="contained" className={classes.messagePrevButton}>
+          <Typography className={classes.messagePrev}>
+            {users.content}
+          </Typography>
+        </Button>
+      ))}
     </Card>
-    </>
   );
 }
--- client/src/components/write/Write.js
+++ client/src/components/write/Write.js
@@ -1,17 +1,24 @@
+import data from "../../data";
+import ComposeCard from "./ComposeCard";
+import DraftCard from "./DraftCard";
+import { makeStyles } from "@material-ui/core";
 
-import data from '../../data';
-import ComposeCard from './ComposeCard';
-import DraftCard from './DraftCard';
-
-
+const useStyles = makeStyles({
+  h1: {
+    fontFamily: "Comfortaa",
+    fontSize: 23,
+    textAlign: "center",
+  },
+});
 
 function Write() {
+  const classes = useStyles();
   return (
     <>
-     <h1> Write </h1>
-     <DraftCard data={data}> </DraftCard>
-     <ComposeCard></ComposeCard> 
-     </>
+      <h1 className={classes.h1}> Write </h1>
+      <DraftCard data={data}> </DraftCard>
+      <ComposeCard></ComposeCard>
+    </>
   );
 }
 
